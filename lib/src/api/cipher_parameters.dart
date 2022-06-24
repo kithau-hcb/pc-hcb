205501e35994f8fd28875c4b15a1395341239582
kithau <kithau@hashcodebook.com>
2022-06-24 10:25:25 +0800

    update

--- lib/src/api/cipher_parameters.dart
+++ lib/src/api/cipher_parameters.dart
@@ -2,7 +2,7 @@
 // This library is dually licensed under LGPL 3 and MPL 2.0.
 // See file LICENSE for more information.
 
-part of api;
+part of pointycastle_hcb.api;
 
 /// All cipher initialization parameters classes implement this.
 abstract class CipherParameters {}
